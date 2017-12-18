58a2241c2bdc3d6d8b51be460fe4499fc24c5327
mrpew <marcelo.reyes.96@gmail.com>
2017-12-18 15:08:32 +0100

    implemented all missing hotkeys

--- xournal_hotkey.sh
+++ xournal_hotkey.sh
@@ -11,23 +11,64 @@ function isfocused {
 	echo "n"
     fi
 }
+
+# execute keystroke <arg>
+function keystroke{
+  keystroke=$1
+  xdotool windowactivate --sync $wid key $keystroke
+}
+# execute keystroke control-<arg>
+function keystroke_cntrl{
+  keystroke="control+"$1
+  xdotool windowactivate --sync $wid key $keystroke
+}
 # execute keystroke control-shift-<arg>
-function keystroke {
+function keystroke_cntrl_shif {
   keystroke="control+shift+"$1
   xdotool windowactivate --sync $wid key $keystroke
 }
 
+
+
 if [ $(isfocused) = "y" ]
 then
   case $1 in
-    ruler)      keystroke L ;;
-    marker)     keystroke H ;;
-    selrect)    keystroke R ;;
-    selarea)    keystroke G ;;
-    shiftvert)  keystroke V ;;
-    pencil)     keystroke P ;;
-    ereaser)    keystroke E ;;
-    image)      keystroke I ;;
-    text)       keystroke T ;;
+    # tools
+    ruler)      keystroke_cntrl_shif L ;;
+    autoshape)  keystroke_cntrl_shif S ;;
+    # selection
+    selrect)    keystroke_cntrl_shif R ;;
+    selarea)    keystroke_cntrl_shif G ;;
+    shiftvert)  keystroke_cntrl_shif V ;;
+    hand)       keystroke_cntrl_shif A ;;
+    # drawing
+    pencil)     keystroke_cntrl_shif P ;;
+    ereaser)    keystroke_cntrl_shif E ;;
+    marker)     keystroke_cntrl_shif H ;;
+    text)       keystroke_cntrl_shif T ;;
+    image)      keystroke_cntrl_shif I ;;
+    # settings
+    font)       keystroke_cntrl_shif F ;;
+    # navigation
+    firstpg)    keystroke_cntrl Home  ;;
+    prevpg)     keystroke_cntrl Left  ;;
+    nextpg)     keystroke_cntrl Right ;;
+    lastpg)     keystroke_cntrl End   ;;
+    # view
+    fullscreen) keystroke F11 ;;
+    # edit
+    undo)       keystroke_cntrl Z ;;
+    redo)       keystroke_cntrl Y ;;
+    cut)        keystroke_cntrl X ;;
+    copy)       keystroke_cntrl C ;;
+    paste)      keystroke_cntrl V ;;
+    delete)     keystroke Delete  ;;
+    # file
+    new)        keystroke_cntrl N ;;
+    open)       keystroke_cntrl O ;;
+    save)       keystroke_cntrl S ;;
+    print)      keystroke_cntrl P ;;
+    exprt)      keystroke_cntrl E ;;
+    quit)       keystroke_cntrl Q ;;
   esac
 fi
